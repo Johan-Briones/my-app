1d8b561b36fa90a5a4fd9366f0999ab4a639ceb3
Johan-Briones <johan.brionesramirez@gmail.com>
2024-05-16 15:55:30 -0600

    fixing app

--- App.js
+++ App.js
@@ -1,20 +1,138 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import firebase from 'firebase/app';
+import firebaseConfig from "./utils/FireBase"
+//import * as firebase from "firebase";
+import "firebase/storage";
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+//firebase.initializeApp(firebaseConfig);
+
+//import * as ImagePicker from 'expo-image-picker';
+//import * as Permissions from 'expo-permissions';
+
+import { Permissions, ImagePicker } from "expo";
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userId: "user73538",
+      imageFirebase: ""
+    };
+  }
+
+  uploadImage = uri => {
+    return new Promise((resolve, reject) => {
+      let xhr = new XMLHttpRequest();
+      xhr.onerror = reject;
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          resolve(xhr.response);
+        }
+      };
+
+      xhr.open("GET", uri);
+      xhr.responseType = "blob";
+      xhr.send();
+    });
+  };
+
+  openGallery = async () => {
+    const resultPermission = await Permissions.askAsync(
+      Permissions.CAMERA_ROLL
+    );
+
+    if (resultPermission) {
+      const resultImagePicker = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [4, 3]
+      });
+
+      if (resultImagePicker.cancelled === false) {
+        const imageUri = resultImagePicker.uri;
+        const { userId } = this.state;
+
+        this.uploadImage(imageUri)
+          .then(resolve => {
+            let ref = firebase
+              .storage()
+              .ref()
+              .child(`images/${userId}`);
+            ref
+              .put(resolve)
+              .then(resolve => {
+                console.log("Imagen subida correctamente");
+              })
+              .catch(error => {
+                console.log("Error al subir la imagen");
+              });
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }
+    }
+  };
+
+  loadImage = async () => {
+    const { userId } = this.state;
+
+    firebase
+      .storage()
+      .ref(`images/${userId}`)
+      .getDownloadURL()
+      .then(resolve => {
+        this.setState({
+          imageFirebase: resolve
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  checkImage = () => {
+    const { imageFirebase } = this.state;
+
+    if (imageFirebase) {
+      return (
+        <Image
+          style={{ width: 300, height: 300 }}
+          source={{ uri: imageFirebase }}
+        />
+      );
+    }
+    return null;
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.checkImage()}
+        <Button
+          onPress={() => this.openGallery()}
+          title="Selecionar una imagen"
+          color="#841584"
+        />
+        <Button
+          onPress={() => this.loadImage()}
+          title="Cargar Imagen"
+          color="#841584"
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
---
+++ utils/FireBase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBdDId0PpQpoPYMwtyMpyRf2eAvz7Sysxc",
+    authDomain: "appspeqtrumbd.firebaseapp.com",
+    projectId: "appspeqtrumbd",
+    storageBucket: "appspeqtrumbd.appspot.com",
+    messagingSenderId: "177239265740",
+    appId: "1:177239265740:web:11dc40c22ac7cf8eb6cd69",
+    measurementId: "G-NW98W2QEK2"
+  };
+
+export default firebaseConfig;
